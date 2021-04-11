200adaa4d743384a93bcd8acd575879eb48b9ada
kautuk kundan <kautukkundan@gmail.com>
2021-04-11 15:45:24 +0530

    [feature] updated directory structure

--- playground/scripts/testSignature.ts
+++
@@ -1,9 +0,0 @@
-import { ethers } from "ethers";
-
-let message =
-  "0x943f74a1a43b3242a06de6491b25d303eb52b9ff6ca475c389c5d7a80d47c46e";
-let secretKey =
-  "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
-
-let wallet = new ethers.Wallet(secretKey);
-console.log(wallet.signMessage(message));
